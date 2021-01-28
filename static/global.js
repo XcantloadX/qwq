ad9ba93f64390cdfeb0980b9d6bc6844d93a2edd
XcantloadX <3188996979@qq.com>
2021-01-29 05:10:39 +0800

    Moved to Jekyll

--- static/global.js
+++
@@ -1,8 +0,0 @@
-function using(url){
-	var js = document.createElement("script");
-	js.src = url;
-	js.type = "text/javascript";
-	document.head.appendChild(js);
-}
-
-using("http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js");
